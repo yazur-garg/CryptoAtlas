c871700828a5e2031803d3eb3abc46ef39964fcc
yazur <gyazur@gmail.com>
2024-07-03 19:27:39 -0400

    Added functionality to dynamically display top 10 coins on home page.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Home = () => {
+
+    const {allCoins, currency} = useContext(CoinContext);
+    const [coinsDisplayed, setCoinsDisplayed] = useState([]);
+
+    useEffect(() => {
+        setCoinsDisplayed(allCoins.slice(0, 10));
+    }, [allCoins]);
+
     return (
         <div className="home">
             <div className="hero">
@@ -21,6 +30,22 @@ const Home = () => {
                     <p style={{textAlign: "center"}}>24H change</p>
                     <p style={{textAlign: "right"}}>Market Cap</p>
                 </div>
+                {
+                    coinsDisplayed.map((coin, index) => (
+                        <div className="table-layout" key={index}>
+                            <p>{coin.market_cap_rank}</p>
+                            <div>
+                                <img src={coin.image} alt="coin" />
+                                <p>{coin.name + " - " + coin.symbol}</p>
+                            </div>
+                            <p>{currency.symbol} {coin.current_price.toLocaleString()}</p>
+                            <p className={coin.price_change_percentage_24h > 0 ? "green" : "red"}>
+                                {coin.price_change_percentage_24h.toFixed(2)}%
+                            </p>
+                            <p style={{textAlign: "right"}}>{currency.symbol} {coin.market_cap.toLocaleString()}</p>                            
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
