9e64dfe56e907aa1be5ed615ef8d4da8b0b8e091
yazur <gyazur@gmail.com>
2024-07-03 17:51:46 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Coin from "./pages/Coin/Coin";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:coinId" element={<Coin />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
