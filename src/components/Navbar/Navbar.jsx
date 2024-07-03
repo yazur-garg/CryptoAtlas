77f1765f7d71b4d35282b869add348c8a266683b
yazur <gyazur@gmail.com>
2024-07-03 19:23:08 -0400

    chore: Update Navbar styling and currency dropdown functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+
+    const {setCurrency} = useContext(CoinContext);
+    const handleCurrencyChange = (e) => {
+        switch(e.target.value) {
+            case "cad":
+                setCurrency({name: "cad", symbol: "CAD $"});
+                break;
+            case "usd":
+                setCurrency({name: "usd", symbol: "$"});
+                break;
+            case "eur":
+                setCurrency({name: "eur", symbol: "€"});
+                break;
+            case "inr":
+                setCurrency({name: "inr", symbol: "₹"});
+                break;
+            default:
+                setCurrency({name: "cad", symbol: "CAD $"});
+        };
+    }
+
     return (
         <div className="navbar">
             <img src={logo} alt="logo" className="logo"/>
@@ -14,11 +36,11 @@ const Navbar = () => {
                 <li>Blog</li>
             </ul>
             <div className="navbar-right">
-                <select>
-                    <option>CAD</option>
-                    <option>USD</option>
-                    <option>EUR</option>
-                    <option>INR</option>
+                <select onChange={handleCurrencyChange}>
+                    <option value="cad">CAD</option>
+                    <option value="usd">USD</option>
+                    <option value="eur">EUR</option>
+                    <option value="inr">INR</option>
                 </select>
                 <button>Sign up</button>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import './index.css'
+import App from './App.jsx'
+
+import CoinContextProvider from "./context/CoinContext";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CoinContextProvider>
+        <App />
+      </CoinContextProvider>
     </Router>
   </React.StrictMode>,
 )
